cbcf5e6d478726af46e8c99920053609f02a9168
Siphiwo <siphiwojulayi462@gmail.com>
2022-11-21 17:05:16 +0200

    initail commit & removal of files

---
+++ src/components/redditListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const redditListContainer = () => {
+  return (
+    <div>redditListContainer</div>
+  )
+}
+
+export default redditListContainer
---
+++ src/components/redditListItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const redditListItem = () => {
+  return (
+    <div>redditListItem</div>
+  )
+}
+
+export default redditListItem
